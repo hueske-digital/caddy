28a4cabb3f84f9a9259d7a41351804ed40e97c5c
Tobias Wiesing <tobias@wiesing.me>
2026-06-12 20:30:36 +0200

    fix(reload): debounce config changes to prevent reload storms
    Ephemeral containers churn config files rapidly, triggering a validate+reload
    per file event. Coalesce bursts by waiting for a 2s quiet window before reloading.

--- build/bin/reload.sh
+++ build/bin/reload.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 while true
 do
+ # Block until the first change.
  inotifywait -r --exclude .swp -e create -e modify -e delete -e move /hosts
+ # Debounce: coalesce rapid bursts (e.g. ephemeral containers) by waiting
+ # for a quiet window before reloading. Each event resets the timer.
+ while inotifywait -r -q -t 2 --exclude .swp -e create -e modify -e delete -e move /hosts >/dev/null 2>&1
+ do
+  :
+ done
  caddy validate --config /etc/caddy/Caddyfile
  if [ $? -eq 0 ]
  then
